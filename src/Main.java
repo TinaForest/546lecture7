676788984942e6d0fb405745d9a54e8bce2ae836
Yunfei Tang <yunfeitang743@gmail.com>
2024-05-15 10:13:51 -0400

    recursion

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,13 @@
 import java.util.*;
 public class Main {
     public static int fibonacci(int i){
-        Stack<Integer> stack = new Stack<>();
-        stack.push(i);
-        int res = 0;
-        while(!stack.isEmpty()) {
-            int x = stack.pop();
-            if( x == 0 || x == 1) {
-                res += 1;
-            } else {
-                stack.push(x-1);
-                stack.push(x-2);
-            }
+        //base case
+        if(i == 0 || i == 1){
+            return 1;
         }
-        return res;
+        return fibonacci(i-2)+fibonacci(i-1);
     }
     public static void main(String[] args) {
-        System.out.println(fibonacci(5));`
+        System.out.println(fibonacci(5));
     }
 }
